696163f3a6e2bcc04ea6720636de55f4a6c12450
Mateusz Skocz <matius1@o2.pl>
2016-06-04 10:47:05 +0200

    db connection + test + driver remember to add driver to buildpath and junit4

---
+++ src/tests/DbUtilTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import util.DbUtil;
+
+public class DbUtilTest {
+	
+	@Test
+	public void test() {
+
+		DbUtil db = new DbUtil();
+		assertNotNull(db.getConnection());
+
+	}
+
+
+}
---
+++ src/util/DbUtil.java
@@ -0,0 +1,64 @@
+package util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DbUtil {
+
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+		System.out.println("DB connection checking");
+		if (connection != null)
+			return connection;
+
+		else {
+			System.out.println("DB connection creating");
+			try {
+				System.out.println("DB properties loading");
+
+				Properties prop = new Properties();
+				InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
+
+				prop.load(inputStream);
+
+				String driver = prop.getProperty("driver");
+				String url = prop.getProperty("url");
+				String user = prop.getProperty("user");
+				String password = prop.getProperty("password");
+				Class.forName(driver);
+				connection = DriverManager.getConnection(url, user, password);
+				System.out.println("Connected to DB");
+
+			} catch (ClassNotFoundException e) {
+				System.err.println("Connection to DB error");
+				e.printStackTrace();
+				return connection;
+			} catch (SQLException e) {
+				System.err.println("Connection to DB error");
+				e.printStackTrace();
+				return connection;
+
+			} catch (FileNotFoundException e) {
+				System.err.println("Connection to DB error: file not found");
+
+				e.printStackTrace();
+				return connection;
+
+			} catch (IOException e) {
+				System.err.println("Connection to DB error");
+				e.printStackTrace();
+				return connection;
+
+			}
+			return connection;
+		}
+
+	}
+
+}
